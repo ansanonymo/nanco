dfa9f36fa79d32acf4e693b70776604c07c9d72f
Tazri <mdtazri@gmail.com>
2024-07-17 05:46:12 +0600

    complete footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
+import Footer from "../components/common/Footer/Footer";
 import Hero from "../components/home/Hero/Hero";
+import HomeServicesSection from "../components/home/HomeServicesSection/HomeServicesSection";
 import HomeClientsSection from "../components/home/clients/HomeClientsSection";
-import HomeServicesSection from "../components/home/services/HomeServicesSection";
 import ProjectCouter from "../components/projectCounter/ProjectCounter";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <ProjectCouter />
       <HomeClientsSection />
       <HomeServicesSection />
+      <Footer />
     </>
   );
 }
