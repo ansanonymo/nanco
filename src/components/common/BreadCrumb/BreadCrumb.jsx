4fa0588c4a70e4281915e9243520196e9df0f014
Tazri <mdtazri@gmail.com>
2024-07-17 17:03:41 +0600

    fixing breadcrumb for large screen

--- src/components/common/BreadCrumb/BreadCrumb.jsx
+++ src/components/common/BreadCrumb/BreadCrumb.jsx
@@ -10,12 +10,12 @@ export default function BreadCrumb({ path, title, pageName }) {
     </>
   );
   return (
-    <div className="container gap-10 sm:gap-10 md:pb-0 bg-no-repeat pb-14 md:gap-0 flex flex-col sm:flex-col md:flex-row md:justify-between items-center">
+    <div className="container gap-10 lg:pb-0 bg-no-repeat pb-14  flex flex-col lg:flex-row lg:justify-between lg:gap-0 items-center">
       <div>
         <h1 className="md:text-5xl lg:text-6xl text-4xl font-semibold text-white">
           {pageName ? pageName : "Please Give Page Name"}
         </h1>
-        <div className="bg-primary hidden w-80 h-[90px] text-lg md:flex flex-col gap-6 mt-7">
+        <div className="bg-primary hidden w-80 h-[90px] text-lg lg:flex flex-col gap-6 mt-7">
           <BreadCrumbLine />
 
           <p className="md:text-lg lg:text-xl pl-10 -mt-2 text-white text-nowrap whitespace-nowrap ">
@@ -26,7 +26,7 @@ export default function BreadCrumb({ path, title, pageName }) {
         </div>
       </div>
       <div className="text-white relative">
-        <div className="w-[30px] hidden md:block aspect-square absolute bg-white right-0 top-0 -translate-y-20">
+        <div className="w-[30px] hidden lg:block aspect-square absolute bg-white right-0 top-0 -translate-y-20">
           <div className="w-full h-full bg-primary -translate-x-1/3 -translate-y-1/3"></div>
         </div>
         {fullPath}
