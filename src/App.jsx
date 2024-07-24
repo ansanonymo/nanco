1440119cd888ee886c47a84acc836413c8b5319b
Tazri <mdtazri@gmail.com>
2024-07-24 19:26:01 +0600

    fixing hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 // import BuissnessLines from "./pages/BuissnessLinesPage";
-// import HomePage from "./pages/HomePage";
-import OtherPage from "./pages/OtherPage";
+import HomePage from "./pages/HomePage";
+// import OtherPage from "./pages/OtherPage";
 
 function App() {
   return (
     <>
-      {/* <HomePage /> */}
-      <OtherPage />
+      <HomePage />
+      {/* <OtherPage /> */}
       {/* <BuissnessLines /> */}
     </>
   );
---
+++ src/pages/FixingHeroPage.jsx
@@ -0,0 +1,19 @@
+import Nav from "./../components/common/Nav/Nav";
+import Hero from "./../components/home/Hero/Hero";
+
+export default function HomePage() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      {/* <ProjectCounter />
+      <HomeServicesSection />
+      <HomeAbout />
+      <HomeProjects />
+      <HomeClientsSection />
+      <HomeOurJourny />
+      <HomeContact />
+      <Footer /> */}
+    </>
+  );
+}
