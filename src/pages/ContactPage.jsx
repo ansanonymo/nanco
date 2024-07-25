4c3e0bab7d13ccea9fb1529f73c9aab7976e41f6
Tazri <mdtazri@gmail.com>
2024-07-25 10:19:16 +0600

    fix google map

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Details from "../components/Contact/Details";
-import GoogleMap from "../components/Contact/Map";
+import GoogleMap from "../components/Contact/GooleMap";
 import HomeContact from "../components/home/HomeContact/HomeContact";
 
 export default function ContactPage() {
   return (
     <>
+      {/* <LocationMap /> */}
       <GoogleMap />
       <Details />
       <HomeContact />
