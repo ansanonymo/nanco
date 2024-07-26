c82175f6bb874b27cfac04712662f466961d1f90
Tazri <mdtazri@gmail.com>
2024-07-26 23:11:42 +0600

    fix project counter

--- src/components/home/ProjectCounter/ProjectCounter.jsx
+++ src/components/home/ProjectCounter/ProjectCounter.jsx
@@ -14,7 +14,7 @@ export default function ProjectCouter() {
         }}
         className="min-w-52 w-full  sm:w-[60%] md:w-[80%] lg:w-[95%] bg-[#ff5e14] text-white bg-center bg-cover bg-no-repeat mx-auto px-3"
       >
-        <div className="grid gap-2 sm:gap-3 md:gap-5 lg:gap-20 py-4 sm:py-7 md:py-12  grid-cols-2 md:grid-cols-2 lg:grid-cols-4 w-fit mx-auto">
+        <div className="grid gap-2 sm:gap-3 sm:gap-x-16 md:gap-y-6 md:gap-x-24 lg:gap-20 py-4 sm:py-7 md:py-12  grid-cols-2 md:grid-cols-2 lg:grid-cols-4 w-fit mx-auto">
           <Counter count="20" name="Awards" Icon={HiOutlineTrophy} />
           <Counter count="900" name="Clients" Icon={PiProjectorScreenChart} />
           <Counter count="10K" name="Projects" Icon={BsBuildings} />
@@ -30,12 +30,12 @@ function Counter({ Icon: PropsIcon, count, name }) {
   return (
     <div className="flex gap-2 sm:gap-2 md:gap-3 items-center">
       {/** icon */}
-      <div className="text-3xl sm:text-2xl md:text-6xl lg:text-6xl">
+      <div className="text-3xl sm:text-2xl md:text-5xl lg:text-6xl">
         {<Icon />}
       </div>
       {/** count and text */}
       <div>
-        <h2 className="text-base sm:text-lg  md:text-4xl lg:text-4xl font-bold">
+        <h2 className="text-base sm:text-lg  md:text-3xl lg:text-4xl font-bold">
           {count}+
         </h2>
         <p className="text-xs sm:text-xs md:text-lg lg:text-lg">{name}</p>
